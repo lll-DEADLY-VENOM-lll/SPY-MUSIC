e4ed3785c2f3068f1e1e5fc5d81f63c2c4d6ed40
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-31 12:22:06 +0530

    0.3 version

--- Spy/plugins/bot/start.py
+++ Spy/plugins/bot/start.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -160,4 +160,5 @@ async def welcome(client, message: Message):
                 await message.stop_propagation()
         except Exception as ex:
             print(ex)
+
 
